8ecbbde73ded92c1c736fcd38665158c63407160
fujidana <53413319+fujidana@users.noreply.github.com>
2022-09-06 10:35:51 +0900

    feat: add symbol navigation in a file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,33 +6,38 @@ All notable changes to the `vscode-igorpro` extension will be documented in this
 
 ### Added
 
-- Make IntelliSence context-sensitive (refer to "IV-1 — Working with Commands / Types of Commands" in the Igor Pro Manual)
+- symbol navigation inside a file, including breadcrumbs. Functions, macros, structures, and constants are marked as symbols.
+- support of a file drop on an editor. The file path is automatically formatted as an `#include` statement.
+
+### Changed
+
+- Make IntelliSence context-sensitive (refer to "IV-1 — Working with Commands / Types of Commands" in the Igor Pro Manual).
   - Hists for built-in operartions are shown only at the beginning word of the command.
   - Hints for built-in functions are not shown at the beginning word of the command.
   - Hists are disabled when a word starts with a slash, `/`. Usually it is a flag of an operation.
-- Support a file drop on an editor (automatically format as an `#include` statement).
 
 ### Fixed
 
-- a minor bug in syntax highlighting rules
+- a minor bug in syntax highlighting rules.
 
-## [1.0.0]
+## [1.0.0] -- initial release
 
-- Initial release. The following features are implemented:
-  - following IntelliSense features for built-in symbols (functions, operations, keywords, etc.)
-    - hovers
-    - completion items
-    - signature helps for functions
-  - virtual document that lists built-in symbols (call "Igor Pro: Open Reference Manual" from Command Pallete)
-  - syntax highlighting
-  - code snippets
-    - flow controls such as `if` and `for`.
-    - declarations such as `Function` and `Macro`
-    - compiler directives such as `#prgama` and `#ifdef`
-  - language configurations
-    - comment toggling (line comment only (`//`). Igor Pro does not support block comments)
-    - bracket definision, auto closing, Autosurrounding (`{}`, `[]`, `()`, `''`, `""`)
-    - indentation rules
+### Added
+
+- following IntelliSense features for built-in symbols (functions, operations, keywords, etc.)
+  - hovers
+  - completion items
+  - signature helps for functions
+- virtual document that lists built-in symbols (call "Igor Pro: Open Reference Manual" from Command Pallete)
+- syntax highlighting
+- code snippets
+  - flow controls such as `if` and `for`.
+  - declarations such as `Function` and `Macro`
+  - compiler directives such as `#prgama` and `#ifdef`
+- language configurations
+  - comment toggling (line comment only (`//`). Igor Pro does not support block comments)
+  - bracket definision, auto closing, Autosurrounding (`{}`, `[]`, `()`, `''`, `""`)
+  - indentation rules
 
 [Unreleased]: https://github.com/fujidana/vscode-igorpro/compare/v1.0.0...HEAD
 [1.0.0]: https://github.com/fujidana/vscode-igorpro/releases/tag/v1.0.0
--- src/userProvider.ts
+++ src/userProvider.ts
@@ -2,14 +2,83 @@ import * as vscode from "vscode";
 import * as igorpro from "./igorpro";
 import { Provider } from "./provider";
 
-export class UserProvider extends Provider implements vscode.DocumentDropEditProvider {
+export class UserProvider extends Provider implements vscode.DocumentSymbolProvider, vscode.DocumentDropEditProvider {
 
     constructor(context: vscode.ExtensionContext) {
         super(context);
 
         context.subscriptions.push(
-            vscode.languages.registerDocumentDropEditProvider(igorpro.IPF_SELECTOR, this)
+            vscode.languages.registerDocumentSymbolProvider(igorpro.IPF_SELECTOR, this),
+            vscode.languages.registerDocumentDropEditProvider(igorpro.IPF_SELECTOR, this),
         );
+    }
+
+    /**
+     * Required implementation of vscode.DocumentSymbolProvider
+     */
+     public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.SymbolInformation[] | vscode.DocumentSymbol[]> {
+        if (token.isCancellationRequested) { return; }
+
+        const results = new Array<vscode.DocumentSymbol>();
+        for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
+            let lineText = document.lineAt(lineIndex).text;
+            let matches: RegExpMatchArray | null;
+            if ((matches = lineText.match(/^(\s*(Static|Override)?\s*\b((?:Str)?Constant)\s*((?:\/\w+\s*)*)\s+)(\w+)/i)) !== null) {
+                results.push(new vscode.DocumentSymbol(
+                    matches[5], '', vscode.SymbolKind.Constant,
+                    new vscode.Range(lineIndex, 0, lineIndex, lineText.length),
+                    new vscode.Range(lineIndex, matches[1].length, lineIndex, matches[1].length + matches[5].length)
+                ));
+            } else if ((matches = lineText.match(/^(\s*(Static)?\s*\b(Structure)\s+)(\w+)\s*(?=;|\/\/|$)/)) !== null) {
+                const lineIndexStart = lineIndex;
+                const name = matches[4];
+                const selectionRange = new vscode.Range(lineIndex, matches[1].length, lineIndex, matches[1].length + matches[4].length);
+                for (lineIndex++; lineIndex < document.lineCount; lineIndex++) {
+                    lineText = document.lineAt(lineIndex).text;
+                    if ((matches = lineText.match(/^\s*(EndStructure)\s*(?=;|\/\/|$)/i)) !== null) {
+                        results.push(new vscode.DocumentSymbol(
+                            name, '', vscode.SymbolKind.Struct,
+                            new vscode.Range(lineIndexStart, 0, lineIndex, lineText.length),
+                            selectionRange)
+                        );
+                        break;
+                    }
+                }
+            } else if ((matches = lineText.match(/\s*((Macro|Proc|Window)\s+)(\w+)\s*(\()((?:(?!\/\/|\)).)*)(\))(?:\s*(:)\s*(\w+))?\s*(?=;|\/\/|$)/i)) !== null) {
+                const lineIndexStart = lineIndex;
+                const name = matches[3];
+                const detail = matches[8] ? matches[8] : '';
+                const selectionRange = new vscode.Range(lineIndex, matches[1].length, lineIndex, matches[1].length + matches[3].length);
+                for (lineIndex++; lineIndex < document.lineCount; lineIndex++) {
+                    lineText = document.lineAt(lineIndex).text;
+                    if ((matches = lineText.match(/^\s*(End(?:Macro)?)\s*(?=;|\/\/|$)/i)) !== null) {
+                        results.push(new vscode.DocumentSymbol(
+                            name, detail, vscode.SymbolKind.Module,
+                            new vscode.Range(lineIndexStart, 0, lineIndex, lineText.length),
+                            selectionRange)
+                        );
+                        break;
+                    }
+                }
+            } else if ((matches = lineText.match(/^(\s*(ThreadSafe)?\s*\b(Static|Override)?\s*\b(Function)\b\s*(?:((?:\/\w+\s*)*)|(\[)((?:(?!\/\/|\]).)*)(\])\s*)?)(\w+)\s*(\()((?:(?!\/\/|\)).)*)(\))(?:\s*(:)\s*(\w+))?\s*(?=;|\/\/|$)/i)) !== null) {
+                const lineIndexStart = lineIndex;
+                const name = matches[9];
+                const detail = matches[14] ? matches[14] : '';
+                const selectionRange = new vscode.Range(lineIndex, matches[1].length, lineIndex, matches[1].length + matches[9].length);
+                for (lineIndex++; lineIndex < document.lineCount; lineIndex++) {
+                    lineText = document.lineAt(lineIndex).text;
+                    if ((matches = lineText.match(/^\s*(End)\s*(?=;|\/\/|$)/i)) !== null) {
+                        results.push(new vscode.DocumentSymbol(
+                            name, detail, vscode.SymbolKind.Function,
+                            new vscode.Range(lineIndexStart, 0, lineIndex, lineText.length),
+                            selectionRange)
+                        );
+                        break;
+                    }
+                }
+            }
+        }
+        return results;
     }
 
     /**
