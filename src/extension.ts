698b0cd743329fd51d93b9fe454dd32f4b14f8ab
fujidana <53413319+fujidana@users.noreply.github.com>
2023-12-22 18:08:45 +0900

    chore: update Node.js packages

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,12 @@ import * as vscode from 'vscode';
 import { SystemProvider } from './systemProvider';
 import { UserProvider } from './userProvider';
 
-// this method is called when your extension is activated
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	new SystemProvider(context);
 	new UserProvider(context);
 }
 
-// this method is called when your extension is deactivated
+// This method is called when your extension is deactivated
 export function deactivate() {}
