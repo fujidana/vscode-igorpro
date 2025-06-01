e1f97a966fb0d652b62af9858122473b6e95420a
fujidana <53413319+fujidana@users.noreply.github.com>
2025-06-01 14:55:42 +0900

    fix: improve indentation for code whose letter case is mixed

--- src/extension.ts
+++ src/extension.ts
@@ -7,9 +7,22 @@ import { UserProvider } from './userProvider';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// For case-insensitive match, the following settings are defined here 
+	// using regular expression leterals, instead of a JSON file 
+	// (`language-configuration.json`) using string leterals.
+	const configuration: vscode.LanguageConfiguration = {
+		indentationRules: {
+			increaseIndentPattern: /^\s*(?:threadsafe\s+)?(?:(?:static|override)\s+)?(?:if|else(?:if)?|do|for|switch|strswitch|try|catch|macro|menu|function|submenu|structure|Proc|Picture|Window)\b/i,
+			decreaseIndentPattern: /^\s*(?:else(?:if)?|while|catch|end(?:if|for|switch|structure|try|macro)?)\b/i,
+		}
+	};
+	context.subscriptions.push(
+		vscode.languages.setLanguageConfiguration('igorpro', configuration)
+	);
+
 	new SystemProvider(context);
 	new UserProvider(context);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
